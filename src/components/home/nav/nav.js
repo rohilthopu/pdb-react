5a8191ee539f5f0edc3b55b584d80825d91d02b2
Rohil Thopu <rohilthopu@gmail.com>
2019-07-26 21:34:38 -0500

    basic navbar implementation

--- src/components/home/nav/nav.js
+++ src/components/home/nav/nav.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import 'bulma/css/bulma.css'
 
-
 const NavBar = () => {
     return (
         <nav className="navbar is-dark" role="navigation">
@@ -9,20 +8,19 @@ const NavBar = () => {
                 <div className="navbar-start">
                     <a className="navbar-item" href="/">
                         <span className="icon"><i className="fas fa-home"/></span>
-                        <span />
-                        <span>Pad DB</span>
+                        <span>PAD DB</span>
                     </a>
-                    <div className="navbar-item has-dropdown is-hoverable">
-                        <a className="navbar-link" href="/">
-                            <span className="icon"><i className="fas fa-globe-americas"/></span>
-                            <span>Data</span>
-                        </a>
-                        <div className="navbar-dropdown">
-                            <a className="navbar-item" href="/">
-                                Monsters
-                            </a>
-                        </div>
-                    </div>
+                    {/*<div className="navbar-item has-dropdown is-hoverable">*/}
+                    {/*    <a className="navbar-link" href="/">*/}
+                    {/*        <span className="icon"><i className="fas fa-globe-americas"/></span>*/}
+                    {/*        <span>Data</span>*/}
+                    {/*    </a>*/}
+                    {/*    <div className="navbar-dropdown">*/}
+                    {/*        <a className="navbar-item" href="/">*/}
+                    {/*            Monsters*/}
+                    {/*        </a>*/}
+                    {/*    </div>*/}
+                    {/*</div>*/}
                 </div>
             </div>
         </nav>
