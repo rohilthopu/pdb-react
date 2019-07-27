f78628d21e406b89e87451660f08fcccbad68648
Rohil Thopu <rohilthopu@gmail.com>
2019-07-26 22:11:08 -0500

    moved some stuff around to have guerrilla as it's own thing

--- src/components/guerrilla/guerrilla.js
+++ src/components/guerrilla/guerrilla.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import NavBar from '../nav/nav.js'
 
 const heroMargin = {
     marginBottom: '2%'
@@ -7,33 +6,8 @@ const heroMargin = {
 
 class Guerrilla extends React.Component {
     render() {
-
-        function getCurrentDate(){
-            let today = new Date();
-            const dd = String(today.getDate()).padStart(2, '0');
-            const mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-            const yyyy = today.getFullYear();
-            today = mm + '/' + dd + '/' + yyyy;
-            return today;
-        }
-
-        function HomeBanner() {
-            return (
-                <section className="hero is-dark" style={heroMargin}>
-                    <div className="hero-body has-text-centered">
-                        <div className="container is-fluid">
-                            <h1 className="title"> Schedule </h1>
-                            <h2 className="subtitle"> Guerrilla Dungeons for {getCurrentDate()} </h2>
-                        </div>
-                    </div>
-                </section>
-            );
-        }
-
         return (
             <div>
-                <NavBar/>
-                <HomeBanner/>
             </div>
         );
     }
--- src/routes/home/home.js
+++ src/routes/home/home.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import NavBar from '../../components/nav/nav'
 import Guerrilla from '../../components/guerrilla/guerrilla.js'
 
-function Home() {
-    return (
-        <Guerrilla/>
-    );
+class Home extends React.Component {
+    render() {
+        function getCurrentDate(){
+            let today = new Date();
+            const dd = String(today.getDate()).padStart(2, '0');
+            const mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+            const yyyy = today.getFullYear();
+            return  mm + '/' + dd + '/' + yyyy;
+        }
+
+        function HomeBanner() {
+            return (
+                <section className="hero is-dark" style={heroMargin}>
+                    <div className="hero-body has-text-centered">
+                        <div className="container is-fluid">
+                            <h1 className="title"> Schedule </h1>
+                            <h2 className="subtitle"> Guerrilla Dungeons for {getCurrentDate()} </h2>
+                        </div>
+                    </div>
+                </section>
+            );
+        }
+        return (
+            <div>
+                <NavBar/>
+                <HomeBanner/>
+                <Guerrilla/>
+            </div>
+        );
+    }
 }
 
 export default Home;
