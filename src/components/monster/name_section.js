d65de3f22f6686e99e7631cf0d41cb312505dbba
Rohil Thopu <rohilthopu@gmail.com>
2019-08-10 14:34:05 -0500

    fetch monster data and push it to the NameSection component

--- src/components/monster/name_section.js
+++ src/components/monster/name_section.js
@@ -1,11 +1,39 @@
 import React, { Component } from "react";
 
 export default class NameSection extends Component {
+    constructor(props) {
+        super(props);
+        this.getPortraitImageLink = this.getPortraitImageLink.bind(this);
+        this.get = this.get.bind(this);
+    }
+
+    getPortraitImageLink() {
+        return (
+            "https://api.pad-db.com/static/padimages/portrait/" +
+            this.get("card_id") +
+            ".png"
+        );
+    }
+
+    get(property) {
+        return this.props.monster[property];
+    }
+
     render() {
         return (
-            <div className="box">
-
-            </div>    
-        )
+            <div className="hero is-dark is-fullheight">
+                <div className="hero-body">
+                    <div className="container is-fluid">
+                        <img
+                            src={this.getPortraitImageLink()}
+                            alt="pad_image"
+                            height="75"
+                            width="75"
+                        />
+                        <div className="title">{this.get('name')}</div>
+                    </div>
+                </div>
+            </div>
+        );
     }
 }
