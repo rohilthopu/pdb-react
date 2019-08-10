99fd58e2e7b977fdb50926b20459cac40e0e1b93
Rohil Thopu <rohilthopu@gmail.com>
2019-08-10 12:48:33 -0500

    updated formatting on search.js and put the search bar size back to small. big just looks weird..

--- src/components/search/search_bar.js
+++ src/components/search/search_bar.js
@@ -6,7 +6,7 @@ class SearchBar extends React.Component {
             <div className="control is-expanded">
                 <input
                     id="search"
-                    className="input is-rounded is-medium"
+                    className="input is-rounded"
                     type="text"
                     placeholder="Ex. attribute = wood and awakenings = 7c, unbindable"
                     onKeyDown={this.props.handleKeyDown}
--- src/routes/search/search.js
+++ src/routes/search/search.js
@@ -1,42 +1,50 @@
-import React from 'react';
-import axios from 'axios';
-import NavBar from '../../components/nav/nav'
-import SearchResults from '../../components/search/search_results';
-import SearchBar from '../../components/search/search_bar';
+import React from "react";
+import axios from "axios";
+import NavBar from "../../components/nav/nav";
+import SearchResults from "../../components/search/search_results";
+import SearchBar from "../../components/search/search_bar";
 
 class Search extends React.Component {
-
     constructor(props) {
         super(props);
-        this.state = {'searchResults': [], 'currentQuery': localStorage.getItem('currentQuery'), 'index': 'monsters', 'showResults': false};
+        this.state = {
+            searchResults: [],
+            currentQuery: localStorage.getItem("currentQuery"),
+            index: "monsters",
+            showResults: false
+        };
         this.handleKeyDown = this.handleKeyDown.bind(this);
         this.storeCurrentSearch = this.storeCurrentSearch.bind(this);
         this.resultsSection = React.createRef();
     }
 
-    handleKeyDown = (e) => {
-        if (e.key === 'Enter') {
-            this.setState({'showResults': true, 'searchResults': []});
-            axios.get('https://api.pad-db.com/search/' + e.target.value)
-                .then((response) => {
-                    this.setState({'searchResults': response.data});
-                    this.resultsSection.current.scrollIntoView({behavior: 'smooth', block: 'start'});
-                }).catch((error) => {
-                    console.log(error)
-                }
-            );
+    handleKeyDown = e => {
+        if (e.key === "Enter") {
+            this.setState({ showResults: true, searchResults: [] });
+            axios
+                .get("https://api.pad-db.com/search/" + e.target.value)
+                .then(response => {
+                    this.setState({ searchResults: response.data });
+                    this.resultsSection.current.scrollIntoView({
+                        behavior: "smooth",
+                        block: "start"
+                    });
+                })
+                .catch(error => {
+                    console.log(error);
+                });
         }
     };
 
-    storeCurrentSearch = (e) => {
-        localStorage.setItem('currentQuery', e.target.value);
-        this.setState({'currentQuery': e.target.value});
+    storeCurrentSearch = e => {
+        localStorage.setItem("currentQuery", e.target.value);
+        this.setState({ currentQuery: e.target.value });
     };
 
     render() {
         return (
             <div>
-                <NavBar/>
+                <NavBar />
                 <section className="hero is-dark is-fullheight-with-navbar">
                     <div className="hero-body">
                         <div className="container">
@@ -44,28 +52,44 @@ class Search extends React.Component {
                                 Welcome to the PAD DB search engine!
                             </p>
                             <p className="subtitle">
-                                This search engine is works through a custom query engine powered by the magic of ElasticSearch.
+                                This search engine is works through a custom
+                                query engine powered by the magic of
+                                ElasticSearch.
                             </p>
                             <div className="field has-addons">
                                 <p className="control">
-                                    <span className="select is-rounded is-medium">
-                                      <select>
-                                        <option>Monsters</option>
-                                      </select>
+                                    <span className="select is-rounded">
+                                        <select>
+                                            <option>Monsters</option>
+                                        </select>
                                     </span>
                                 </p>
-                                <SearchBar handleKeyDown={this.handleKeyDown} storeCurrentSearch={this.storeCurrentSearch} currentQuery={this.state.currentQuery} />
+                                <SearchBar
+                                    handleKeyDown={this.handleKeyDown}
+                                    storeCurrentSearch={this.storeCurrentSearch}
+                                    currentQuery={this.state.currentQuery}
+                                />
                             </div>
                             <p className="subtitle top-margin">
-                                Check out the guide at <a className="has-text-link" href={'/search-guide/'}>/search-guide/</a> to learn about how to query data.
+                                Check out the guide at{" "}
+                                <a
+                                    className="has-text-link"
+                                    href={"/search-guide/"}
+                                >
+                                    /search-guide/
+                                </a>{" "}
+                                to learn about how to query data.
                             </p>
                         </div>
                     </div>
                 </section>
 
                 <div ref={this.resultsSection}>
-                    <SearchResults searchResults={this.state.searchResults}
-                                   index={this.state.index} showResults={this.state.showResults} />
+                    <SearchResults
+                        searchResults={this.state.searchResults}
+                        index={this.state.index}
+                        showResults={this.state.showResults}
+                    />
                 </div>
             </div>
         );
