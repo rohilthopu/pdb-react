718102835a31f28658151d911c24cbc586002b53
Rohil Thopu <rohilthopu@gmail.com>
2019-08-27 20:26:27 -0500

    removed typo

--- src/components/monster/collab.js
+++ src/components/monster/collab.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class Collab extends Component {
     render() {
         return (
-             <div />;
+             <div />
         );
     }
 }
