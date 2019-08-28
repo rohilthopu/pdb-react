4f898cb7d05b6cd07706d23c757ac07ed704e7d8
Rohil Thopu <rohilthopu@gmail.com>
2019-08-28 00:58:59 -0500

    fixed a bug where only single elements were being returned

--- src/routes/search/search.js
+++ src/routes/search/search.js
@@ -24,7 +24,11 @@ class Search extends React.Component {
         axios
             .get("https://api.pad-db.com/search/monsters/" + value)
             .then(response => {
-                this.setState({ searchResults: response.data });
+                if (response.data.constructor === Object) {
+                    this.setState({ searchResults: [response.data] });
+                } else {
+                    this.setState({ searchResults: response.data });
+                }
                 this.resultsSection.current.scrollIntoView({
                     behavior: "smooth",
                     block: "start"
